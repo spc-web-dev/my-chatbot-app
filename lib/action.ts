2a7577ba9f7c127cb422ca653085055f1b608b58
spc-web-dev <itspc2023@gmial.com>
2025-06-09 13:24:42 +0700

    change version of model

--- lib/action.ts
+++ lib/action.ts
@@ -6,7 +6,7 @@ import { streamText } from "ai";
 
 export async function generate(prompt:string){
 const result = await streamText({
-    model: google('gemini-2.0-flash'),
+    model: google('gemini-2.5-flash-preview-04-17'),
     prompt: prompt,
   });
   return {textStream: result.textStream}; 
